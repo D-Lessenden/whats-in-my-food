8fe67871acf84cb85a442ac81856dc4c2aef7165
Daniel Lessenden <dless27@gmail.com>
2020-10-19 10:44:20 -0500

    Add action in controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,11 @@
 class FoodsController < ApplicationController
   def index
-  end 
+    search = params[:q]
+    conn = Faraday.new(url: 'https://api.nal.usda.gov') do |faraday|
+      faraday.headers['API-Key'] = ENV['API-KEY']
+    end
+    response = conn.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API-KEY']}&query=#{search}")
+    @json = JSON.parse(response.body, symbolize_names: true)
+    # binding.pry
+  end
 end
