cbb689698741e2161275841e93b6b2c942ce2431
Daniel Lessenden <dless27@gmail.com>
2020-10-19 11:06:46 -0500

    Test for proper info

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -7,6 +7,10 @@ describe "Welcome page" do
     fill_in :q, with: "sweet potatoes"
     click_on "Search"
     expect(current_path).to eq('/foods')
-    expect(page).to have_content("There are #{} results for your search!")
+    expect(page).to have_content("There are 50 results for your search!")
+    expect(page).to have_content("Description: SWEET POTATOES")
+    expect(page).to have_content("Brand Owner: The Pictsweet Company")
+    expect(page).to have_content("GTIN/UPC: 070560951975")
+    expect(page).to have_content("Ingredients: SWEET POTATOES.")
   end
 end
